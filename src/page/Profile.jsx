efde137ad5f47d5f16b856e65f29cfde1f9edf52
it-havetic <it.havetic@gmail.com>
2024-09-16 17:14:42 +0600

    profile page done

--- src/page/Profile.jsx
+++ src/page/Profile.jsx
@@ -6,95 +6,73 @@ import { AiOutlineProduct } from 'react-icons/ai'
 import Footer from '../components/Footer'
 import Slider from 'react-slick'
 
-import b1 from "/assets/mockup/m2.jpg"; 
-import b10 from "/assets/mockup/m11.jpg"; 
+import b1 from "/assets/mockup/m2.jpg";
+import b10 from "/assets/mockup/m11.jpg";
 import b14 from "/assets/mockup/m7.jpg";
 import { HiDotsHorizontal } from 'react-icons/hi'
+import { FaBriefcase, FaHammer, FaTrophy, FaUsers, FaUserTie } from 'react-icons/fa'
+import { MdOutlineConstruction } from 'react-icons/md'
 
 
 const Profile = () => {
-    let navigate = useNavigate();
-    const [isItemVisible, setIsItemVisible] = useState(false);
+  let navigate = useNavigate();
+  const [isItemVisible, setIsItemVisible] = useState(false);
 
-    const handleToggle = () => {
-      setIsItemVisible(!isItemVisible);
-    };
-    let settings = {
-        dots: false,
-        arrows: false,
-        infinite: true,
-        speed: 2000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        cssEase: "linear",
-        autoplay: true,
-        autoplaySpeed: 4000,
-    };
-    let bannerImg = [b10, b1, b14];
-    return (
-      <div className="h-screen pb-9 pt-8 bg-gray-100 grid grid-cols-1 grid-rows-8 gap-2">
-        <div className="flex items-center justify-between absolute top-0 left-0  h-8 bg-[#000000] ">
-          <Link
-            className="flex items-center justify-center w-[20%] text-xs"
-            to="/"
-          >
-            <img
-              className="w-4/5 drop-shadow-custom"
-              src="/assets/bdl.png"
-              alt=""
-            />
-          </Link>
-          <Marquee
-            speed={20}
-            direction="left"
-            pauseOnHover={true}
-            reverse={true}
-            gradient={false}
-            gradientColor={["#6FA710"]}
-            className="h-8 "
-          >
-            <div className="flex h-full">
-              <img className=" h-8" src={slogan} alt="" />
-              <img className=" h-8" src={slogan} alt="" />
-              <img className=" h-8" src={slogan} alt="" />
-            </div>
-          </Marquee>
-          <div
-          className="navItem w-[20%] z-10 h-full flex justify-center items-center  text-[#F15B26] relative "
-          onClick={handleToggle}
+  const handleToggle = () => {
+    setIsItemVisible(!isItemVisible);
+  };
+  let settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    cssEase: "linear",
+    autoplay: true,
+    autoplaySpeed: 4000,
+  };
+  let bannerImg = [b10, b1, b14];
+  return (
+    <div className="h-screen pb-8 pt-8 bg-gray-100 grid grid-cols-1 grid-rows-8 gap-0">
+      <div className="flex items-center justify-between absolute top-0 left-0  h-8 bg-[#000000] ">
+        <Link
+          className="flex items-center justify-center w-[20%] text-xs"
+          to="/"
+        >
+          <img
+            className="w-4/5 drop-shadow-custom"
+            src="/assets/bdl.png"
+            alt=""
+          />
+        </Link>
+        <Marquee
+          speed={20}
+          direction="left"
+          pauseOnHover={true}
+          reverse={true}
+          gradient={false}
+          gradientColor={["#6FA710"]}
+          className="h-8 "
+        >
+          <div className="flex h-full">
+            <img className=" h-8" src={slogan} alt="" />
+            <img className=" h-8" src={slogan} alt="" />
+            <img className=" h-8" src={slogan} alt="" />
+          </div>
+        </Marquee>
+        <div
+          className="navItem w-[20%] z-10 h-full flex justify-center items-center  text-[#F15B26] relative"
+          onClick={() => navigate("/menu")}
         >
           <HiDotsHorizontal />
-          <div
-            className={`item px-5 py-2 bg-slate-300 absolute right-0 top-full transition-all duration-300 ${
-              isItemVisible
-                ? "top-full !opacity-100 !visible"
-                : "top-[140%] opacity-0 invisible"
-            }`}
-          >
-            <ul className="text-right">
-              <li>
-                <Link className="text-[#F15B26] capitalize" to="/contact">
-                  appointment
-                </Link>
-              </li>
-              <li>
-                <Link className="text-[#F15B26] capitalize" to="/profile">
-                  profile
-                </Link>
-              </li>
-              <li>
-                <Link className="text-[#F15B26] capitalize" to="/blog">
-                  academy
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        </div>
 
-        <div className="row-span-2 bg-slate-400 relative">
-          {/* <Slider className="md:h-full h-full " {...settings}>
+        </div>
+      </div>
+
+      <div className="row-span-2 bg-slate-400 relative">
+        {/* <Slider className="md:h-full h-full " {...settings}>
             {bannerImg.map((item, i) => (
               <div
                 key={i}
@@ -109,40 +87,128 @@ const Profile = () => {
               </div>
             ))}
           </Slider> */}
-          <video
-            className="w-full h-full object-cover"
-            controls
-            autoPlay
-            loop
-            muted
-            src="/profileMid.mp4"
-          />
-          <div className="absolute -bottom-5 left-1 z-10">
-            <h2 className="textShadow -mb-6 ml-9 text-sm companyText text-[#00c3ff] font-bold opacity-100 ">
-              Company
-            </h2>
-            <h2 className="textShadow text-6xl text-[#c3fe00] font-bold opacity-100">
-              Profile
-            </h2>
+        <video
+          className="w-full h-full object-cover"
+          controls
+          autoPlay
+          loop
+          muted
+          src="/profileMid.mp4"
+        />
+        <div className="absolute -bottom-5 left-1 z-10">
+          <h2 className="textShadow -mb-6 ml-9 text-sm companyText text-[#F15B26] font-bold opacity-100 ">
+            Company
+          </h2>
+          <h2 className="textShadow text-6xl text-[#8ac249] font-bold opacity-100">
+            Profile
+          </h2>
+        </div>
+      </div>
+
+      <div className="row-span-3 grid grid-cols-3 px-7 pt-9 pb-3  gap-1 bg-[#33420a]">
+        {/* Complete Project */}
+        <div className='flex flex-col justify-evenly items-center'>
+          <h2 className="textShadow text-sm companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
+            Complete <br /> Projects
+          </h2>
+          <div
+            className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
+            style={{ "--value": 100, "--thickness": "4px" }}
+            role="progressbar">
+            <FaBriefcase className='text-[#8ac249]' />
+            <p className='text-[#8ac249]'>156</p>
           </div>
         </div>
-        <div className="row-span-3 bg-white ">
-          <img
-            className="w-full h-full object-contain"
-            src="/profiledata.png"
-          />
+
+        {/* Satisfied Clients */}
+        <div className='flex flex-col justify-evenly items-center'>
+          <h2 className="textShadow text-sm companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
+            Satisfied <br /> Clients
+          </h2>
+          <div
+            className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
+            style={{ "--value": 100, "--thickness": "4px" }}
+            role="progressbar">
+            <FaUserTie className='text-[#8ac249]' />
+            <p className='text-[#8ac249]'>156</p>
+          </div>
         </div>
-        <div className="row-span-3 bg-slate-700 px-4 py-2 text-justify text-white">
-          <h2 className='font-bold'>BD Luminaries</h2>
-  <p className="text-sm">
-    Welcome to our company profile! Explore our journey, projects, and services. We strive to deliver the highest quality products and innovations, continuously pushing the boundaries of excellence.
-  </p>
-</div>
 
+        {/* Under Construction */}
+        <div className='flex flex-col justify-evenly items-center'>
+          <h2 className="textShadow text-sm companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
+            Under <br /> Construction
+          </h2>
+          <div
+            className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
+            style={{ "--value": 70, "--thickness": "4px" }}
+            role="progressbar">
+            <MdOutlineConstruction className='text-[#8ac249]' />
+            <p className='text-[#8ac249]'>6</p>
+          </div>
+        </div>
 
-        <Footer />
+        {/* Ongoing Projects */}
+        <div className='flex flex-col justify-evenly items-center'>
+          <h2 className="textShadow text-sm companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
+            Ongoing <br /> Projects
+          </h2>
+          <div
+            className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
+            style={{ "--value": 75, "--thickness": "4px" }}
+            role="progressbar">
+            <FaHammer className='text-[#8ac249]' />
+            <p className='text-[#8ac249]'>50</p>
+          </div>
+        </div>
+
+        {/* Team Members */}
+        <div className='flex flex-col justify-evenly items-center'>
+          <h2 className="textShadow text-sm companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
+            Team <br /> Members
+          </h2>
+          <div
+            className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
+            style={{ "--value": 90, "--thickness": "4px" }}
+            role="progressbar">
+            <FaUsers className='text-[#8ac249]' />
+            <p className='text-[#8ac249]'>20</p>
+          </div>
+        </div>
+
+        {/* Awards Won */}
+        <div className='flex flex-col justify-evenly items-center'>
+          <h2 className="textShadow text-sm companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
+            Awards <br /> Won
+          </h2>
+          <div
+            className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
+            style={{ "--value": 80, "--thickness": "4px" }}
+            role="progressbar">
+            <FaTrophy className='text-[#8ac249]' />
+            <p className='text-[#8ac249]'>16</p>
+          </div>
+        </div>
       </div>
-    );
+
+      <div className="row-span-3 bg-[#33420a] px-4  text-justify text-[#eeeeee] overflow-hidden relative">
+        <p className="text-sm ">
+           <br /> our journey, projects, and services. We <br /> strive to deliver the highest quality <br /> products and innovations, continuously <br /> pushing the boundaries of excellence. <br />
+          Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Porro perspiciatis <br /> animi aut nemo id magnam <br /> magni dolor quis fuga sed <br /> ducimus, eos eligendi aliquam <br /> obcaecati inventore, officiis <br /> commodi ex error.
+          Welcome <br /> to our company pro the.
+        </p>
+
+        <p className="text-lg font-sign">Javed A. Bhuiya</p>
+
+        <p className="text-xs">Chairman & CEO</p>
+        
+        <img className='h-[110%] absolute bottom-0 right-2 opacity-5' src="/taufik.png" alt="" />
+      </div>
+
+
+      <Footer />
+    </div>
+  );
 }
 
 export default Profile
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      'sign': ['Dancing Script', 'cursive']
+    },
     extend: {
       dropShadow: {
         custom: "0px 0px 11px #000000",
@@ -9,6 +12,9 @@ export default {
       // textShadow: {
       //   custom: "0px 0px 3px rgba(255,255,255,0.74)",
       // },
+      clipPath: {
+        'custom-shape': 'polygon(78% 13.5%, 70.4% 29.3%, 70.4% 50%, 67.4% 68.3%, 72.3% 86.8%, 68.1% 100%, 0% 100%, 0% 0%, 78.1% 0%)',
+      }
     },
   },
   plugins: [
@@ -24,5 +30,7 @@ export default {
       });
     },
     require("tailwindcss-textshadow"),
+    require('daisyui'),
+    require('tailwind-clip-path'),
   ],
 };
