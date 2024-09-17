533b4290f5f977a74f25e87f309506b9a5e671a4
Nayan-Islam <islamnayan588@gmail.com>
2024-09-17 18:07:29 +0600

    profile done

---
+++ src/components/LogoMarquee.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Marquee from "react-marquee-slider";
+
+const logos = [
+  { src: "/public/client/BSRM.png", alt: "BSRM" },
+  { src: "/public/client/Bengal.png", alt: "Bengal" },
+  { src: "/public/client/Citygroup.png", alt: "Citygroup" },
+  { src: "/public/client/Gazi Group.png", alt: "Gazi Group" },
+  { src: "/public/client/Partex Star Group.png", alt: "Partex Star Group" },
+  { src: "/public/client/Pran-RFL Group.avif", alt: "Pran-RFL Group" },
+  { src: "/public/client/Salma Group.webp", alt: "Salma Group" },
+  { src: "/public/client/Westin.png", alt: "Westin.png" },
+  { src: "/public/client/epic.png", alt: "epic.png" },
+  // Add more logos here
+];
+
+const LogoMarquee = () => {
+  return (
+    <div className="marquee-container">
+      <Marquee
+        velocity={10}
+      >
+        {logos.map((logo, index) => (
+          <img
+            key={index}
+            src={logo.src}
+            alt={logo.alt}
+            className="m-0 mx-3 h-16"
+          />
+        ))}
+      </Marquee>
+    </div>
+  );
+};
+
+export default LogoMarquee;
--- src/page/Profile.jsx
+++ src/page/Profile.jsx
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
-import Marquee from 'react-fast-marquee'
-import { Link, useNavigate } from 'react-router-dom'
-import slogan from '/assets/slogan.png'
-import { AiOutlineProduct } from 'react-icons/ai'
-import Footer from '../components/Footer'
-import Slider from 'react-slick'
+import React, { useState } from "react";
+import Marquee from "react-fast-marquee";
+import { Link, useNavigate } from "react-router-dom";
+import Footer from "../components/Footer";
+import slogan from "/assets/slogan.png";
 
-import b1 from "/assets/mockup/m2.jpg";
+import {
+  FaBriefcase,
+  FaHammer,
+  FaTrophy,
+  FaUsers,
+  FaUserTie,
+} from "react-icons/fa";
+import { HiDotsHorizontal } from "react-icons/hi";
+import { MdOutlineConstruction } from "react-icons/md";
+import LogoMarquee from "../components/LogoMarquee";
 import b10 from "/assets/mockup/m11.jpg";
+import b1 from "/assets/mockup/m2.jpg";
 import b14 from "/assets/mockup/m7.jpg";
-import { HiDotsHorizontal } from 'react-icons/hi'
-import { FaBriefcase, FaHammer, FaTrophy, FaUsers, FaUserTie } from 'react-icons/fa'
-import { MdOutlineConstruction } from 'react-icons/md'
-
 
 const Profile = () => {
   let navigate = useNavigate();
@@ -35,7 +39,7 @@ const Profile = () => {
   };
   let bannerImg = [b10, b1, b14];
   return (
-    <div className="h-screen pb-8 pt-8 bg-gray-100 grid grid-cols-1 grid-rows-8 gap-0">
+    <div className="h-screen pb-8 pt-8 bg-gray-100 grid grid-cols-1 grid-rows-9 gap-0">
       <div className="flex items-center justify-between absolute top-0 left-0  h-8 bg-[#000000] ">
         <Link
           className="flex items-center justify-center w-[20%] text-xs"
@@ -67,11 +71,10 @@ const Profile = () => {
           onClick={() => navigate("/menu")}
         >
           <HiDotsHorizontal />
-
         </div>
       </div>
 
-      <div className="row-span-2 bg-slate-400 relative">
+      <div className="row-span-2 bg-slate-400">
         {/* <Slider className="md:h-full h-full " {...settings}>
             {bannerImg.map((item, i) => (
               <div
@@ -95,120 +98,135 @@ const Profile = () => {
           muted
           src="/profileMid.mp4"
         />
-        <div className="absolute -bottom-5 left-1 z-10">
-          <h2 className="textShadow -mb-6 ml-9 text-sm companyText text-[#F15B26] font-bold opacity-100 ">
+      </div>
+      <div className="row-span-3 bg-[#33420a] px-4 pt-5  text-justify text-[#eeeeee] overflow-scroll relative">
+        <div className="absolute top-4 left-4 z-10">
+          <h2 className="textShadow -mb-5 ml-[28px] text-xs companyText text-[#F15B26] font-bold opacity-100 ">
             Company
           </h2>
-          <h2 className="textShadow text-6xl text-[#8ac249] font-bold opacity-100">
+          <h2 className="textShadow text-5xl text-[#8ac249] font-bold opacity-100">
             Profile
           </h2>
         </div>
-      </div>
+        <p className="text-xs">
+          <br /> <br /> <br /> products and innovations, continuously <br />{" "}
+          pushing the boundaries of excellence. <br />
+          Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Porro
+          perspiciatis <br /> animi aut nemo id magnam <br /> magni dolor quis
+          fuga sed <br /> ducimus, eos eligendi aliquam <br /> obcaecati
+          inventore, officiis <br /> commodi ex error. Welcome <br /> to our
+          company pro the.
+        </p>
 
-      <div className="row-span-3 grid grid-cols-3 grid-rows-2 px-2 pt-9 pb-3  gap-1 bg-[#33420a]">
+        <p className="text-lg font-sign">Javed A. Bhuiya</p>
+
+        <p className="text-xs">Chairman & CEO</p>
+
+        <img
+          className="h-[100%] rounded-b-2xl absolute bottom-0 right-5 opacity-100"
+          src="/taufik.png"
+          alt=""
+        />
+      </div>
+      <div className="row-span-3 grid grid-cols-3 grid-rows-2 px-2 pt-4 pb-0  gap-1 bg-[#33420a]">
         {/* Complete Project */}
-        <div className='flex flex-col justify-evenly items-center'>
+        <div className="flex flex-col justify-evenly items-center">
           <h2 className="textShadow text-xs companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
             Completed Projects
           </h2>
           <div
             className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
             style={{ "--value": 100, "--thickness": "4px" }}
-            role="progressbar">
-            <FaBriefcase className='text-[#8ac249]' />
-            <p className='text-[#8ac249]'>156</p>
+            role="progressbar"
+          >
+            <FaBriefcase className="text-[#8ac249]" />
+            <p className="text-[#8ac249]">156</p>
           </div>
         </div>
 
         {/* Satisfied Clients */}
-        <div className='flex flex-col justify-evenly items-center'>
+        <div className="flex flex-col justify-evenly items-center">
           <h2 className="textShadow text-xs companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
             Satisfied Clients
           </h2>
           <div
             className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
             style={{ "--value": 100, "--thickness": "4px" }}
-            role="progressbar">
-            <FaUserTie className='text-[#8ac249]' />
-            <p className='text-[#8ac249]'>156</p>
+            role="progressbar"
+          >
+            <FaUserTie className="text-[#8ac249]" />
+            <p className="text-[#8ac249]">156</p>
           </div>
         </div>
 
         {/* Under Construction */}
-        <div className='flex flex-col justify-evenly items-center'>
+        <div className="flex flex-col justify-evenly items-center">
           <h2 className="textShadow text-xs companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
             Under Construction
           </h2>
           <div
             className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
             style={{ "--value": 70, "--thickness": "4px" }}
-            role="progressbar">
-            <MdOutlineConstruction className='text-[#8ac249]' />
-            <p className='text-[#8ac249]'>6</p>
+            role="progressbar"
+          >
+            <MdOutlineConstruction className="text-[#8ac249]" />
+            <p className="text-[#8ac249]">6</p>
           </div>
         </div>
 
         {/* Ongoing Projects */}
-        <div className='flex flex-col justify-evenly items-center'>
+        <div className="flex flex-col justify-evenly items-center">
           <h2 className="textShadow text-xs companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
             Ongoing Projects
           </h2>
           <div
             className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
             style={{ "--value": 75, "--thickness": "4px" }}
-            role="progressbar">
-            <FaHammer className='text-[#8ac249]' />
-            <p className='text-[#8ac249]'>50</p>
+            role="progressbar"
+          >
+            <FaHammer className="text-[#8ac249]" />
+            <p className="text-[#8ac249]">50</p>
           </div>
         </div>
 
         {/* Team Members */}
-        <div className='flex flex-col justify-evenly items-center'>
+        <div className="flex flex-col justify-evenly items-center">
           <h2 className="textShadow text-xs companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
             Team Members
           </h2>
           <div
             className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
             style={{ "--value": 90, "--thickness": "4px" }}
-            role="progressbar">
-            <FaUsers className='text-[#8ac249]' />
-            <p className='text-[#8ac249]'>20</p>
+            role="progressbar"
+          >
+            <FaUsers className="text-[#8ac249]" />
+            <p className="text-[#8ac249]">20</p>
           </div>
         </div>
 
         {/* Awards Won */}
-        <div className='flex flex-col justify-evenly items-center'>
+        <div className="flex flex-col justify-evenly items-center">
           <h2 className="textShadow text-xs companyText text-[#ffffff] font-bold opacity-100 text-center leading-5">
             Awards Won
           </h2>
           <div
             className="radial-progress bg-[#8ac24900] text-[#f15c2688] font-bold text-xl border-0 flex flex-col items-center justify-center shadow-lg"
             style={{ "--value": 80, "--thickness": "4px" }}
-            role="progressbar">
-            <FaTrophy className='text-[#8ac249]' />
-            <p className='text-[#8ac249]'>16</p>
+            role="progressbar"
+          >
+            <FaTrophy className="text-[#8ac249]" />
+            <p className="text-[#8ac249]">16</p>
           </div>
         </div>
       </div>
 
-      <div className="row-span-3 bg-[#33420a] px-4  text-justify text-[#eeeeee] overflow-scroll relative">
-        <p className="text-xs ">
-           <br /> our journey, projects, and services. We <br /> strive to deliver the highest quality <br /> products and innovations, continuously <br /> pushing the boundaries of excellence. <br />
-          Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Porro perspiciatis <br /> animi aut nemo id magnam <br /> magni dolor quis fuga sed <br /> ducimus, eos eligendi aliquam <br /> obcaecati inventore, officiis <br /> commodi ex error.
-          Welcome <br /> to our company pro the.
-        </p>
-
-        <p className="text-lg font-sign">Javed A. Bhuiya</p>
-
-        <p className="text-xs">Chairman & CEO</p>
-        
-        <img className='h-[110%] absolute bottom-0 right-2 opacity-10' src="/taufik.png" alt="" />
+      <div className="row-span-1 bg-[#33420a] flex items-center">
+        <LogoMarquee />
       </div>
-
 
       <Footer />
     </div>
   );
-}
+};
 
-export default Profile
+export default Profile;
