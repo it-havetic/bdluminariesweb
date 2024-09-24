dc77e00d774e39ad7375b4b45206ff23507d6d27
it-havetic <it.havetic@gmail.com>
2024-09-24 10:59:27 +0600

    profile logo issue fixed

--- src/components/LogoMarquee.jsx
+++ src/components/LogoMarquee.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import Marquee from "react-marquee-slider";
 
 const logos = [
-  { src: "/public/client/BSRM.png", alt: "BSRM" },
-  { src: "/public/client/Bengal.png", alt: "Bengal" },
-  { src: "/public/client/Citygroup.png", alt: "Citygroup" },
-  { src: "/public/client/Gazi Group.png", alt: "Gazi Group" },
-  { src: "/public/client/Partex Star Group.png", alt: "Partex Star Group" },
-  { src: "/public/client/Pran-RFL Group.avif", alt: "Pran-RFL Group" },
-  { src: "/public/client/Salma Group.webp", alt: "Salma Group" },
-  { src: "/public/client/Westin.png", alt: "Westin.png" },
-  { src: "/public/client/epic.png", alt: "epic.png" },
+  { src: "/client/BSRM.png", alt: "BSRM" },
+  { src: "/client/Bengal.png", alt: "Bengal" },
+  { src: "/client/Citygroup.png", alt: "Citygroup" },
+  { src: "/client/Gazi Group.png", alt: "Gazi Group" },
+  { src: "/client/Partex Star Group.png", alt: "Partex Star Group" },
+  { src: "/client/Pran-RFL Group.avif", alt: "Pran-RFL Group" },
+  { src: "/client/Salma Group.webp", alt: "Salma Group" },
+  { src: "/client/Westin.png", alt: "Westin.png" },
+  { src: "/client/epic.png", alt: "epic.png" },
   // Add more logos here
 ];
 
