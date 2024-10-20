47539fcd8db1ccd6814da25d1d18d59f9de7c34d
Nayan-Islam <islamnayan588@gmail.com>
2024-10-20 15:02:22 +0600

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "BD Luminaries",
+        short_name: "BDL",
+        description: "A React app with PWA support using Vite",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/bdl.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/bdl.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
