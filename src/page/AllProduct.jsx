94513b5fcdc5473393e062b80256db2c784f086c
Nayan-Islam <islamnayan588@gmail.com>
2024-10-20 17:15:01 +0600

    pwa

--- src/page/AllProduct.jsx
+++ src/page/AllProduct.jsx
@@ -20,7 +20,7 @@ const AllProduct = () => {
       <div className='overflow-y-scroll flex flex-wrap justify-between gap-y-3 p-2 pr-3'>
         {images.map((img, index) => (
           <Link to="/product/detail" className="w-[48%] inline-block" key={index}>
-            <img className="w-full rounded-md" src={img} alt={`Product ${index + 1}`} />
+            <img className="w-[100%] rounded-md" src={img} alt={`Product ${index + 1}`} />
           </Link>
         ))}
       </div>
